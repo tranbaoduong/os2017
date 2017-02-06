6e020b0748095ae796e1080ba8e85a1f76f679d6
tranbaoduong <jim.tran.287@gmail.com>
2017-02-06 21:58:33 +0700

    04.practical.work.fork.exec.c

---
+++ 04.practical.work.fork.exec.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+ int main() 
+ {
+ 	printf("Main before fork()\n");
+ 	int pid = fork();
+ 	if (pid == 0) {
+ 		printf("I am child after fork(), launching ps -ef\n");
+ 		char *args[]= { "/bin/ps", "-ef" , NULL};
+ 		execvp("/bin/ps", args);
+ 		printf("Finished launching ps -ef\n");
+ 	}
+ 	else printf("I am parent after fork(), child is %d\n", pid);
+ 	pid = fork();
+ 	if (pid == 0) {
+ 		printf("I am child after fork(), launching free -h\n");
+ 		char *args[]= { "free", "-h" , NULL};
+ 		execvp("free", args);
+ 		printf("Finished launching free -h\n");
+ 	}
+ 	else printf("I am parent after fork(), child is %d\n", pid);
+ 	return 0;
+ } 
